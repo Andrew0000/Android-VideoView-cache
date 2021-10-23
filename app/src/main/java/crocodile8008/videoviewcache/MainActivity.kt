603228e5e37b1b8ea4f1328c110392e72938b4c3
crocodile8 <crocodile8000@gmail.com>
2021-10-23 18:05:15 +0300

    RecyclerActivity

--- app/src/main/java/crocodile8008/videoviewcache/MainActivity.kt
+++ app/src/main/java/crocodile8008/videoviewcache/MainActivity.kt
@@ -1,34 +1,63 @@
 package crocodile8008.videoviewcache
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import android.widget.VideoView
 import crocodile8008.videoviewcache.lib.VideoViewCacheFacade
 import crocodile8008.videoviewcache.lib.VideoViewCached
 import crocodile8008.videoviewcache.lib.playUrl
+import crocodile8008.videoviewcache.lib.stop
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cachedVideoView: VideoViewCached
     private lateinit var videoView: VideoView
+    private lateinit var btnClean: View
+    private lateinit var btnRecycler: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         cachedVideoView = findViewById(R.id.cachedVideoView)
         videoView = findViewById(R.id.videoView)
+        btnClean = findViewById(R.id.btnClean)
+        btnRecycler = findViewById(R.id.btnRecycler)
 
-        VideoViewCacheFacade.logEnabled = true
+        VideoViewCacheFacade.logEnabled = BuildConfig.DEBUG
 
-        cachedVideoView.mpPreparedListener = MediaPlayer.OnPreparedListener {
-            it.isLooping = true
+        cachedVideoView.apply {
+            mpPreparedListener = MediaPlayer.OnPreparedListener {
+                it.isLooping = true
+            }
+            cachedVideoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4")
         }
-        cachedVideoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4")
 
-        videoView.setOnPreparedListener {
-            it.isLooping = true
+        videoView.apply {
+            setOnPreparedListener {
+                it.isLooping = true
+            }
+            videoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4")
         }
-        videoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4")
+
+        btnClean.setOnClickListener {
+            cachedVideoView.stop()
+            videoView.stop()
+
+            val deleted = File(VideoViewCacheFacade.getOutputDirPath(this)).deleteRecursively()
+            Toast.makeText(this, "deleted: $deleted", Toast.LENGTH_SHORT).show()
+
+            cachedVideoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4")
+            videoView.playUrl("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4")
+        }
+
+        btnRecycler.setOnClickListener {
+            startActivity(Intent(this, RecyclerActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/crocodile8008/videoviewcache/RecyclerActivity.kt
@@ -0,0 +1,67 @@
+package crocodile8008.videoviewcache
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegate
+import crocodile8008.videoviewcache.lib.VideoViewCached
+import crocodile8008.videoviewcache.lib.playUrl
+import crocodile8008.videoviewcache.lib.stop
+
+class RecyclerActivity : AppCompatActivity() {
+
+    private lateinit var recycler: RecyclerView
+
+    private val adapter by lazy {
+        ListDelegationAdapter(
+            createVideoViewAdapterDelegate(),
+            createVideoViewCachedAdapterDelegate(),
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+        recycler = findViewById(R.id.recycler)
+        recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        recycler.adapter = adapter
+
+        adapter.items = listOf(
+            UrlForVideoView("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"),
+            UrlForVideoViewCached("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4"),
+            UrlForVideoView("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4"),
+            UrlForVideoViewCached("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4"),
+            UrlForVideoView("https://download.samplelib.com/mp4/sample-5s.mp4"),
+            UrlForVideoViewCached("https://download.samplelib.com/mp4/sample-10s.mp4"),
+            UrlForVideoView("https://download.samplelib.com/mp4/sample-15s.mp4"),
+            UrlForVideoViewCached("https://download.samplelib.com/mp4/sample-30s.mp4"),
+        )
+    }
+
+    private fun createVideoViewAdapterDelegate() = adapterDelegate<UrlForVideoView, Any>(R.layout.item_video_view) {
+        val videoView : VideoView = findViewById(R.id.itemVideoView)
+        bind {
+            videoView.stop()
+            videoView.playUrl(item.url)
+        }
+    }
+
+    private fun createVideoViewCachedAdapterDelegate() = adapterDelegate<UrlForVideoViewCached, Any>(R.layout.item_video_view_cached) {
+        val itemVideoViewCached : VideoViewCached = findViewById(R.id.itemVideoViewCached)
+        bind {
+            itemVideoViewCached.stop()
+            itemVideoViewCached.playUrl(item.url)
+        }
+    }
+}
+
+private data class UrlForVideoView(
+    val url: String,
+)
+
+private data class UrlForVideoViewCached(
+    val url: String,
+)
